125ab1aa91ed24add776836cd90009b752f16118
mlouward <mlouward@gmail.com>
2020-12-12 17:34:48 +0100

    from a graph and a dead player, returns list of possible impostor duos

--- Part 2/impostors.py
+++ Part 2/impostors.py
@@ -1,4 +1,6 @@
 from node import Node
+from itertools import combinations, takewhile
+
 
 def process(node_info):
     '''
@@ -16,9 +18,37 @@ def load_graph(path):
     '''
     with open(path) as f:
         data_list = map(str.strip, f.readlines())
-    adjacency_list = [Node(i, j) for i, j in map(process, data_list)]
+    adjacency_list = [(i, j) for i, j in map(process, data_list)]
 
     return adjacency_list
 
+def probable_impostors(graph, dead_player: int):
+    '''
+    A graph is a list of adjacency lists for each node in the graph.
+    This method returns a list of pairs of probable impostors
+    knowing that dead_player is reported dead.
+    '''
+    # We build the first list of probable impostors from the adjacency
+    # list of dead_player
+
+    impostors_combinations = set()
+    imp_list1 = graph[dead_player][1]
+    
+    for impostor in imp_list1:
+        # We can clear out from the impostor 2 the dead player,
+        # the impostor 1 and the players imp1 has seen. We use a set
+        # for the O(1) lookup time and to remove duplicates.
+        clear = {dead_player, impostor, *graph[impostor][1]}
+        suspects = [i for i in range(len(graph)) if i not in clear]
+        for i, c in enumerate(combinations([impostor] + suspects, 2)):
+            if i < len(suspects):
+                print(tuple(c))
+                impostors_combinations.add(tuple(c))
+        
+    # remove duplicates tuples ((1, 3) and (3, 1) are the same)
+    return set((a,b) if a<=b else (b,a) for a,b in impostors_combinations)
+
 if __name__ == "__main__":
-    print(load_graph('graph.txt'))
+    graph = load_graph('graph.txt')
+    print(graph)
+    print(probable_impostors(graph, 0))
--- Part 2/node.py
+++ Part 2/node.py
@@ -1,7 +1,8 @@
 class Node:
     def __init__(self, name, neighbours=[]):
         ''' initializes a node of a graph with a name and a list of neighbours
-        (as an adjacency list).
+        (as an adjacency list). Each node is a player, and the neighbours
+        is the list of other players seen by the player.
         '''
         self.name = name
         self.neighbours = neighbours
