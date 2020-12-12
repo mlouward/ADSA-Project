e2b09122c6157c88155a45564b920cdee6e9213d
mlouward <mlouward@gmail.com>
2020-12-12 19:16:42 +0100

    Same problem but with Depth-First search limited at 2

--- Part 2/impostors.py
+++ Part 2/impostors.py
@@ -1,8 +1,7 @@
-from node import Node
 from itertools import combinations
 
 
-def process(node_info):
+def process(node_info: str):
     '''
     Returns a node name and its neighbours from
     a string with format <name: neigh1, neigh2, ...>
@@ -12,7 +11,7 @@ def process(node_info):
     neighbours = [int(i) for i in a[1].split(',')]
     return name, neighbours
 
-def load_graph(path):
+def load_graph(path: str):
     '''
     Returns a graph made from a text file
     in which each node lists its neighbours.
@@ -23,30 +22,39 @@ def load_graph(path):
 
     return adjacency_list
 
+def dls(graph, root, max_depth):
+    res = set()
+
+    def _dls(graph, root, parent, max_depth):
+        if max_depth == 0:
+            res.update([(parent, root)])
+            return
+
+        if max_depth != 1:
+            possible = graph[root][1]
+        else:
+            # At last step, we want those who didn't see the
+            # impostors_combinations so we remove them of the suspects
+            possible = [i for i in range(len(graph))
+                        if i not in [root] + graph[root][1]]
+        for child in possible:
+            if child != parent:
+                _dls(graph, child, root, max_depth - 1)
+
+    _dls(graph, root, root, max_depth)
+    return res
+
 def probable_impostors(graph, dead_player: int):
     '''
     A graph is a list of adjacency lists for each node in the graph.
     This method returns a list of pairs of probable impostors
     knowing that dead_player is reported dead.
     '''
-    # We build the first list of probable impostors from the adjacency
-    # list of dead_player
-
-    impostors_combinations = set()
-
     # We go through the list of players having seen dead_player
-    for impostor in graph[dead_player][1]:
-        # We can clear out from the impostor 2 the dead player,
-        # the impostor 1 and the players imp1 has seen. We use a set
-        # for the O(1) lookup time and to remove duplicates.
-        clear = {dead_player, impostor, *graph[impostor][1]}
-        suspects = [i for i in range(len(graph)) if i not in clear]
-        for i, c in enumerate(combinations([impostor] + suspects, 2)):
-            if i < len(suspects):
-                print(tuple(c))
-                impostors_combinations.add(tuple(c))
-        
-    # remove duplicates tuples ((1, 3) and (3, 1) are the same)
+    # using Depth First Search with limited depth with dead_player as root
+    impostors_combinations = dls(graph, dead_player, max_depth=2)
+
+    # remove duplicates tuples ((1, 3) and (3, 1) are the same for example)
     return set((a,b) if a<=b else (b,a) for a,b in impostors_combinations)
 
 if __name__ == "__main__":
