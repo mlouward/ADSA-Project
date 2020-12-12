627fba181d7866d4b60a793cde71979d1db88518
mlouward <mlouward@gmail.com>
2020-12-12 19:20:21 +0100

    Refactoring

--- Part 2/impostors.py
+++ Part 2/impostors.py
@@ -1,8 +1,7 @@
-from node import Node
 from itertools import combinations
 
 
-def process(node_info):
+def process(node_info: str):
     '''
     Returns a node name and its neighbours from
     a string with format <name: neigh1, neigh2, ...>
@@ -12,7 +11,7 @@ def process(node_info):
     neighbours = [int(i) for i in a[1].split(',')]
     return name, neighbours
 
-def load_graph(path):
+def load_graph(path: str):
     '''
     Returns a graph made from a text file
     in which each node lists its neighbours.
